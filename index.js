7246e8a043ca85ec996675e37d99d0afa052246b
talitast <talitatravassos.tt@gmail.com>
2020-01-12 14:13:21 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Todo API')
+})
+
+const port = process.env.PORT || 4000 
+
+app.listen(port, () => console.log(`listening on port ${port}`));
